018cd7dc6b0c022367597e6dc46d20a719a180ae
Diaa <ayman.sharkawy609>
2023-02-20 17:18:14 +0200

    finish project

--- app/src/main/java/com/diaa/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/diaa/cryptocurrencyapp/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +13,8 @@ import androidx.compose.material3.Text
 
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
@@ -26,29 +29,33 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCurrencyAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.CoinListScreen.route,
+                AnimatedVisibility(visible = true) {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
                     ) {
-                        composable(
-                            route = Screen.CoinListScreen.route
+                        val navController = rememberNavController()
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screen.CoinListScreen.route,
                         ) {
-                            CoinListScreen(navController)
-                        }
-                        composable(
-                            route = Screen.CoinDetailScreen.route + "/{${Constants.PARAM_COIN_ID}}"
-                        ) {
-                            CoinDetailScreen()
+                            composable(
+                                route = Screen.CoinListScreen.route
+                            ) {
+                                CoinListScreen(navController)
+                            }
+                            composable(
+                                route = Screen.CoinDetailScreen.route + "/{${Constants.PARAM_COIN_ID}}"
+                            ) {
+                                CoinDetailScreen()
+                            }
                         }
                     }
                 }
